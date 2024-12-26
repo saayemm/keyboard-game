d3c3388e86ae47bd83ae93427a44a8d93f3c31e1
Saayemm <sayemdcc@gmail.com>
2024-12-26 10:54:46 +0600

    random index generate

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+function continueGame(){
+    const alphabet = getARandomAlphabet()
+    console.log(alphabet);
+}
+
 function play(){
     hideElemntById("home")
     showElemntById("playground")
+
+    continueGame()
 }
--- utility.js
+++ utility.js
@@ -7,3 +7,15 @@ function showElemntById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove("hidden")
 }
+
+function getARandomAlphabet(){
+    const alphabetstrr = "abcdefghijklmnopqrstuvwxyz"
+    const alphabets = alphabetstrr.split("")
+    // console.log(alphabets);
+    
+    const randomNumber = Math.random()*25;
+    const indexNum = Math.round(randomNumber)
+
+    const alphabet = alphabets[indexNum]
+    return alphabet;
+}
